5610d288ce4b8ce1a86236b0bb8a2544d10e3600
Akash Singh <akashsingh.dev1@gmail.com>
2025-12-01 09:41:26 +0530

    the first commit of this repo this repo will contain react ui components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Greeting from "./components/Greeting/Greeting";
+
+function App() {
+  return (
+    <>
+      <Greeting /> <hr />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Greeting/Greeting.jsx
@@ -0,0 +1,5 @@
+import "./Greeting.css";
+
+export default function Greeting() {
+  return <h1 className="greeting">NAMASTE WORLD</h1>;
+}
